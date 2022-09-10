f713dee7362e51e7ed310dad7520f91ba8ac8570
Adson Gomes Oliveira <agdev@DESKTOP-0N655A3.localdomain>
2022-09-10 01:41:23 -0300

    <feat> Route categories done

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 
+import routes from './routes/index.routes';
+
 const app = express();
 
 app.use(helmet());
 app.use(cors());
-app.use();
+app.use('/categories', routes.CategoriesRoutes);
 
 export default app;
