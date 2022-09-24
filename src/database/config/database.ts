d6c30a71f2f3ce136809d5b86bdce446d39c3c6f
Adson Gomes Oliveira <agdev@DESKTOP-0N655A3.localdomain>
2022-09-24 13:11:30 -0300

    <fix> Fixing env (1)

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -4,13 +4,13 @@ import ProjectModel from '../models/project.model';
 import ProjectCategoryModel from '../models/projectCategory.model';
 
 const config = {
-  username: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || '@DesertLirium123',
-  database: process.env.DB_NAME || 'devago',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3306,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   models: [ CategoryModel, ProjectModel, ProjectCategoryModel ],
-  dialect: 'mysql',
+  dialect: 'postgres',
 }
 
 module.exports = config;
