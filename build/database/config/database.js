ab7f62fe0a5fc50638499a8c7c39d2da20297587
Adson Gomes Oliveira <agdev@DESKTOP-0N655A3.localdomain>
2022-09-24 13:32:15 -0300

    <fix> Fixing env (2)

--- build/database/config/database.js
+++ build/database/config/database.js
@@ -8,12 +8,12 @@ const category_model_1 = __importDefault(require("../models/category.model"));
 const project_model_1 = __importDefault(require("../models/project.model"));
 const projectCategory_model_1 = __importDefault(require("../models/projectCategory.model"));
 const config = {
-    username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || '@DesertLirium123',
-    database: process.env.DB_NAME || 'devago',
-    host: process.env.DB_HOST || 'localhost',
-    port: Number(process.env.DB_PORT) || 3306,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     models: [category_model_1.default, project_model_1.default, projectCategory_model_1.default],
-    dialect: 'mysql',
+    dialect: 'postgres',
 };
 module.exports = config;
