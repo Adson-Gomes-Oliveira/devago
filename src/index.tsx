3082767bedecdde841336a4da1df0105650a14b2
Adson Gomes Oliveira <agdev@DESKTOP-0N655A3.localdomain>
2022-09-26 09:53:48 -0300

    <settings> Router setup completed

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import './index.css';
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
