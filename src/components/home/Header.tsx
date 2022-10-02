1b0ff146f2803b6cb5c839ebee57c1ac51bb187d
adson_gomes_oliveira <contato@devago.com.br>
2022-10-02 15:01:16 -0300

    <media-queries> Header Responsive

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,11 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+
 import LogoDevago from '../../assets/images/devago-light-logo.svg';
 
 import './style.header.css';
 
 export default function Header(): JSX.Element {
+  const navigate = useNavigate();
+
   return (
     <header className="main-header">
       <img src={LogoDevago} alt="Logomarca Devago" />
+      <nav className="header-nav">
+        <ul>
+          <li onClick={() => navigate('/')}>Home</li>
+          <li onClick={() => navigate('/projects')}>Projetos</li>
+          <li onClick={() => navigate('/contact')}>Contato</li>
+        </ul>
+      </nav>
     </header>
   );
 }
