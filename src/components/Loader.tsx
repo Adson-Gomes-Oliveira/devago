182ba99d7bc735c4be4e2f5ac8632a11f2ad7b9e
adson_gomes_oliveira <contato@devago.com.br>
2022-10-03 03:02:54 -0300

    <fix> removed image from loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -13,7 +13,6 @@ export default function Loader(): JSX.Element {
 
   return (
     <section className={`loader ${!isLoading && 'hidden'}`}>
-      <img src={DevagoLogo} alt="devago-logo" />
       <RingLoader
         color="#1486A8"
         loading={true}
